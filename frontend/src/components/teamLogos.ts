2069b011c0db08e33a5176df82a4e5c7c0ef10b9
Francesco <ffevoli@gmail.com>
2025-05-12 16:37:42 +0200

    Update teamLogos.ts (was broken lol)

--- frontend/src/components/teamLogos.ts
+++ frontend/src/components/teamLogos.ts
@@ -1,10 +1,12 @@
 export const teamLogos: { [team: string]: string } = {
-    "Red Bull Racing": "https://upload.wikimedia.org/wikipedia/en/8/8e/Red_Bull_Racing_logo.svg",
-    "Mercedes": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Mercedes-Benz_logo_2010.svg/320px-Mercedes-Benz_logo_2010.svg.png",
-    "Ferrari": "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Scuderia_Ferrari_Logo.svg/320px-Scuderia_Ferrari_Logo.svg.png",
-    "McLaren": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/McLaren_Racing_logo.svg/320px-McLaren_Racing_logo.svg.png",
-    "Aston Martin": "https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Aston_Martin_F1_Logo.svg/320px-Aston_Martin_F1_Logo.svg.png",
-    "Alpine": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/Alpine_F1_Team_logo.svg/320px-Alpine_F1_Team_logo.svg.png",
-    "Haas F1 Team": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/Haas_F1_Team_logo.svg/320px-Haas_F1_Team_logo.svg.png",
-    "Williams": "https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Williams_Grand_Prix_Engineering_logo.svg/320px-Williams_Grand_Prix_Engineering_logo.svg.png",
+    "Red Bull Racing": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/red%20bull",
+    "Mercedes": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/mercedes",
+    "Ferrari": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/ferrari",
+    "McLaren": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/mclaren",
+    "Aston Martin": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/aston%20martin",
+    "Alpine": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/alpine",
+    "Haas F1 Team": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/haas",
+    "Williams": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/williams",
+    "RB":"https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/rb",
+    "Kick Sauber": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/kick%20sauber"
 };
